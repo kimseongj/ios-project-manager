390934467e9371a8806c5a9c614988d97db81d29
vetto <gzzjk159@naver.com>
2023-05-30 16:50:15 +0900

    feat: firebase 추가

---
+++ ProjectManager/ProjectManager/Source/Utility/FirebaseManager.swift
@@ -0,0 +1,11 @@
+//
+//  FirebaseManager.swift
+//  ProjectManager
+//
+//  Created by songjun, vetto on 2023/05/30.
+//
+
+import Foundation
+import FirebaseDatabase
+
+let ref = Database.database().reference()
